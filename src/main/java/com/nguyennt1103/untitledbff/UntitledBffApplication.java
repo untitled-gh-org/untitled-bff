508ef5eab051892d88a966e63784232a62856f03
nguyennt1103 <nguyen.nt1103@gmail.com>
2026-05-12 20:55:31 +0700

    Initial commit

---
+++ src/main/java/com/nguyennt1103/untitledbff/UntitledBffApplication.java
@@ -0,0 +1,13 @@
+package com.nguyennt1103.untitledbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntitledBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nguyennt1103/untitledbff/UntitledBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyennt1103.untitledbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
